83d99f879264bb1cc4991314f4d6fa72d5030721
QixYuanmeng <63216559+QixYuanmeng@users.noreply.github.com>
2024-04-28 15:57:31 +0800

    Update default.nix

--- vars/default.nix
+++ vars/default.nix
@@ -1,6 +1,6 @@
 {lib}: {
-  username = "ryan";
-  userfullname = "Ryan Yin";
-  useremail = "xiaoyin_c@qq.com";
+  username = "qix";
+  userfullname = "qixyuan";
+  useremail = "3286406540@qq.com";
   networking = import ./networking.nix {inherit lib;};
 }
