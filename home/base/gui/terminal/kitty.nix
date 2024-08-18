683722ed575d6c6d89306d0d48f7573113417b19
QixYuanmeng <3286046540@qq.com>
2024-08-18 12:36:18 +0800

    for use

--- home/base/gui/terminal/kitty.nix
+++ home/base/gui/terminal/kitty.nix
@@ -40,6 +40,7 @@
       macos_option_as_alt = true; # Option key acts as Alt on macOS
       enable_audio_bell = false;
       tab_bar_edge = "top"; # tab bar on top
+      confirm_os_window_close = 0;
       #  To resolve issues:
       #    1. https://github.com/ryan4yin/nix-config/issues/26
       #    2. https://github.com/ryan4yin/nix-config/issues/8
